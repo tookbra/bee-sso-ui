672f3d6c35bb27d6a01b9bb148c373463e641f9e
tookbra@gmail.com <cql159357>
2019-02-28 23:59:30 +0800

    :tada: Initial commit

---
+++ src/views/layout/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="bee-container">
+        <header class="nav-header"></header>
+        <div class="bee-main">
+            <div class="nav-left">
+            </div>
+            <div class="bee-context">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "index"
+    }
+</script>
+
+<style scoped>
+
+</style>
